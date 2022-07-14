e2b727085a2ffd812b88ab14af9ac65ca7d7a137
Wongsakon <kotori373510@gmail.com>
2022-07-14 19:54:27 +0700

    Answer

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+
+func main() {
+// 	Coding Quiz
+// var whatIsIt string
+// secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+// sd, _ := base64.StdEncoding.DecodeString(secret)
+
+secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+sd, _ := base64.StdEncoding.DecodeString(secret)
+whatIsIt := fmt.Sprintf("%q",reverseByte(sd))
+
+fmt.Println(whatIsIt)
+
+// Answer: "Join:us:at:LINE:MAN:Wongnai"
+}
+
+func reverseByte(input []byte) []byte {
+if len(input) == 0 {
+	return input
+}
+return append(reverseByte(input[1:]), input[0]) 
+}
