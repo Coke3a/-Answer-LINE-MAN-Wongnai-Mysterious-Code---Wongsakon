99221132a8fff5cc933af453ecee6d2072b8f01d
Wongsakon <kotori373510@gmail.com>
2022-07-14 19:55:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -5,25 +5,24 @@ import (
 	"fmt"
 )
 
-
 func main() {
-// 	Coding Quiz
-// var whatIsIt string
-// secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
-// sd, _ := base64.StdEncoding.DecodeString(secret)
+	// 	Coding Quiz
+	// var whatIsIt string
+	// secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+	// sd, _ := base64.StdEncoding.DecodeString(secret)
 
-secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
-sd, _ := base64.StdEncoding.DecodeString(secret)
-whatIsIt := fmt.Sprintf("%q",reverseByte(sd))
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+	whatIsIt := fmt.Sprintf("%q", reverseByte(sd))
 
-fmt.Println(whatIsIt)
+	fmt.Println(whatIsIt)
 
-// Answer: "Join:us:at:LINE:MAN:Wongnai"
+	// Answer: "Join:us:at:LINE:MAN:Wongnai"
 }
 
 func reverseByte(input []byte) []byte {
-if len(input) == 0 {
-	return input
-}
-return append(reverseByte(input[1:]), input[0]) 
+	if len(input) == 0 {
+		return input
+	}
+	return append(reverseByte(input[1:]), input[0])
 }
